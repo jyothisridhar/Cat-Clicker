534049f6105ee6a8eb378dc2d9a8818251ed5655
Jyothi Sridhar <Jyothi Sridhar>
2016-02-05 23:11:52 +0530

    modify event listeners for display kitty and clicks on cat

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var names = ["Soft-kitty", "Warm-kitty", "Ball-of-fur", "Happy-kitty", "Sleepy-kitty"];
+var counter = 0;
 
 for (var i = 0; i < names.length; i++) {
 	var kittyName = names[i];
@@ -10,11 +11,19 @@ for (var i = 0; i < names.length; i++) {
 		return function(){
 			var filename = "images/" + nameCopy + ".jpg";
 			document.getElementById('kitty-img').src = filename;
-			// document.getElementById('kitty-display').appendChild()
-			console.log(filename);
+			document.getElementById('catName');
+			catName.textContent = nameCopy;
 		}
 	})(kittyName));
 
 	var parent = document.getElementById('kitty-list');
 	parent.appendChild(elem);
 }
+
+var kittyPic = document.getElementById('kitty-img');
+kittyPic.addEventListener('click', function(){
+	// return function() {
+		counter++;
+		document.getElementById('clicks').textContent = "No of clicks:" + counter;
+	//}
+});
